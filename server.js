0d3fe539d765ad3ac606f4ca8509ad3b842f2a60
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-11-20 17:41:25 -0300

    sfghjk

--- server.js
+++ server.js
@@ -1,41 +1,11 @@
 import express from "express";
-
-const posts = [
-  {
-    descricao: "Uma foto teste",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 1,
-  },
-  {
-    descricao: "Gato fofo dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 2,
-  },
-  {
-    descricao: "Gato fofo dormindo",
-    imagem: "https://placecats.com/millie/300/150",
-    id: 3,
-  },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+routes(app)
 
 app.listen(3000, () => {
   console.log("Servidor pronto");
 });
 
-app.get("/posts", (req, res) => {
-  res.status(200).json(posts);
-});
 
-function buscarPostsPorID(id) {
-  return posts.findIndex((post) => {
-    return post.id == Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = buscarPostsPorID(req.params.id);
-  res.status(200).json(posts[index]);
-});
